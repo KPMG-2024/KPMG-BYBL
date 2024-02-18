37f36a9595222ad396405db7262d3cd0038c9e7b
chestnut1717 <psh56750855@gmail.com>
2024-02-18 06:38:05 +0000

    Feat: 대시보드 링크 수정

--- FE/src/components/Dashboard.jsx
+++ FE/src/components/Dashboard.jsx
@@ -6,7 +6,7 @@ const Dashboard = () => {
     <main className='relative flex flex-col w-screen h-screen p-1 overflow-hidden dark:bg-light-grey'>
       <iframe
         title='대시보드'
-        src='https://public.tableau.com/views/2_17079191820660/real?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link?:showVizHome=no&:embed=true'
+        src='https://public.tableau.com/views/kpmg2_17082376126980/sheet7?:language=en-US&publish=yes&:sid=&:display_count=n&:origin=viz_share_link?:showVizHome=no&:embed=true'
         className='w-full h-full'
       />
     </main>
